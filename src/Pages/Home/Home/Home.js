e70fdc08dc3f7b535cf34f02e936db5fa375ae76
iamraihan <trueraihan@gmail.com>
2022-04-17 02:00:12 +0600

    navbar done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className=" bg-gradient-to-r from-cyan-100 to-blue-100" style={{ height: 'calc(100vh - 80px)' }}>
+            <button>Book Appointment</button>
+            <button>Get Special Offer</button>
+
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className='md:flex justify-between max-w-screen-xl	 mx-auto'>
+        <nav className='md:flex justify-between max-w-screen-xl h-20 items-center	 mx-auto'>
             <div>
                 <Link to='/'>Hoffman</Link>
             </div>
             <div className='md:flex'>
-                <Link className='ml-5' to='/'>Home</Link>
-                <Link className='ml-5' to='/services'>Services</Link>
-                <Link className='ml-5' to='/blogs'>Blogs</Link>
-                <Link className='ml-5' to='/about-me'>About Me</Link>
+                <Link className='ml-5 list-none' to='/'><li>Home</li></Link>
+                <Link className='ml-5 list-none' to='/services'><li>Services</li></Link>
+                <Link className='ml-5 list-none' to='/blogs'><li>Blogs</li></Link>
+                <Link className='ml-5 list-none' to='/about-me'><li>About Me</li></Link>
             </div>
         </nav>
     );
