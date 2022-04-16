7e9ea44337a6680a0578bf3e58eca2ebf41f8811
iamraihan <trueraihan@gmail.com>
2022-04-17 01:27:32 +0600

    navbar added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar</h2>
-        </div>
+        <nav className='md:flex justify-between max-w-screen-xl	 mx-auto'>
+            <div>
+                <Link to='/'>Hoffman</Link>
+            </div>
+            <div className='md:flex'>
+                <Link className='ml-5' to='/'>Home</Link>
+                <Link className='ml-5' to='/services'>Services</Link>
+                <Link className='ml-5' to='/blogs'>Blogs</Link>
+                <Link className='ml-5' to='/about-me'>About Me</Link>
+            </div>
+        </nav>
     );
 };
 
