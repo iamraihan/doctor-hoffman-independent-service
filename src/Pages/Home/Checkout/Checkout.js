00d5997f946cbdbcc761be1bfe36c92fbecc336a
iamraihan <trueraihan@gmail.com>
2022-04-18 21:10:31 +0600

    custom link added

--- src/Pages/Home/Checkout/Checkout.js
+++ src/Pages/Home/Checkout/Checkout.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+
+import { useForm } from "react-hook-form";
 
 const Checkout = () => {
     // const { productId } = useParams()
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
-            <h2>Product checkout</h2>
-        </div>
+        <div>This is checkout page.</div>
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        // <form onSubmit={handleSubmit(onSubmit)}>
+        //     {/* register your input into the hook by invoking the "register" function */}
+        //     <input defaultValue="test" {...register("example")} />
+
+        //     {/* include validation with required or other standard HTML validation rules */}
+        //     <input {...register("exampleRequired", { required: true })} />
+        //     {/* errors will return when field validation fails  */}
+        //     {errors.exampleRequired && <span>This field is required</span>}
+
+        //     <input type="submit" />
+        // </form>
     );
 };
 
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { signOut } from 'firebase/auth';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
+import CustomLink from '../../CustomLInk/CustomLink';
 
 const Navbar = () => {
     const [user, loading, error] = useAuthState(auth);
+    const [open, setOpen] = useState(false)
 
     const logout = () => {
         signOut(auth);
     };
     return (
-        <nav className='md:flex justify-between max-w-screen-xl h-20 items-center	 mx-auto'>
-            <div>
-                <Link to='/'>Hoffman</Link>
+        <div className=''>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6  md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
             </div>
-            <div className='md:flex'>
-                <Link className='ml-5 list-none' to='/'><li>Home</li></Link>
-                <Link className='ml-5 list-none' to='/services'><li>Services</li></Link>
-                <Link className='ml-5 list-none' to='/blogs'><li>Blogs</li></Link>
-                <Link className='ml-5 list-none' to='/about-me'><li>About Me</li></Link>
-                {
-                    user ? <Link to='/login'><button onClick={logout} className=" ml-5 px-7 py-1 bg-sky-500 text-white rounded-md hover:bg-sky-700">LogOut</button></Link>
-                        :
-                        <Link to='/login'><button className=" ml-5 px-7 py-1 bg-sky-500 text-white rounded-md hover:bg-sky-700">Login</button></Link>
-                }
-            </div>
-        </nav>
+            <nav className={`md:flex md:justify-between bg-white  md:max-w-screen-xl md:h-20 items-center md:mx-auto absolute md:static  duration-500 ease-in ${open ? 'top-6' : 'top-[-120px]'}`}>
+                <div>
+                    <Link to='/'>Hoffman</Link>
+                </div>
+                <div className='flex '>
+                    <CustomLink className='ml-5 pb-' to='/about-me'>About Me</CustomLink>
+                    <CustomLink className='ml-5 pb-' to='/'>Home</CustomLink>
+                    <CustomLink className='ml-5 pb-' to='/services'>Services</CustomLink>
+                    <CustomLink className='ml-5 pb-' to='/blogs'>Blogs</CustomLink>
+                    {
+                        user ? <CustomLink to='/login'><button onClick={logout} className=" ml-5 px-7 py-1 bg-sky-500 text-white rounded-md hover:bg-sky-700">LogOut</button></CustomLink>
+                            :
+                            <CustomLink to='/login'><button className=" ml-5 px-7 py-1 bg-sky-500 text-white rounded-md hover:bg-sky-700">Login</button></CustomLink>
+                    }
+                </div>
+            </nav>
+        </div>
     );
 };
 
